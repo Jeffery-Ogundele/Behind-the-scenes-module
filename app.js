bef78e7f787f5e2cb3e0026c8c378fe387da4b77
Ogundele Jeffery <jefferyogundele7@gmail.com>
2023-11-03 09:47:33 +0100

    var vs let and const, Understanding Blockscope

---
+++ app.js
@@ -0,0 +1,23 @@
+let name = 'Max';
+
+
+if (name === 'Max') {
+    let hobbies = ['Sports','Cooking'];
+    console.log (hobbies);
+}
+// block is simply created by the curly braces
+
+
+function greet () {
+    let age = 30;
+    let name = 'Manuel';
+    console.log(name, age, hobbies);
+}
+
+
+
+console.log(name, hobbies);
+//What does deprecated mean??
+
+
+greet();
