cfa0c64852cd82e94cb18459388c8a414382f9fc
Ogundele Jeffery <jefferyogundele7@gmail.com>
2023-11-03 09:57:44 +0100

    Understanding Hoisting

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-let name = 'Max';
+// let name = 'Max';
 
 
-if (name === 'Max') {
-    let hobbies = ['Sports','Cooking'];
-    console.log (hobbies);
-}
-// block is simply created by the curly braces
+// if (name === 'Max') {
+//     let hobbies = ['Sports','Cooking'];
+//     console.log (hobbies);
+// }
+// // block is simply created by the curly braces
 
 
-function greet () {
-    let age = 30;
-    let name = 'Manuel';
-    console.log(name, age, hobbies);
-}
+// function greet () {
+//     let age = 30;
+//     let name = 'Manuel';
+//     console.log(name, age, hobbies);
+// }
 
 
 
-console.log(name, hobbies);
-//What does deprecated mean??
+// console.log(name, hobbies);
+// //What does deprecated mean??
 
 
-greet();
+// greet();
+
+//Hoisting invisibly declares the variable at the beginning of the script but sers var to undefined but let and const is just declared  
+console.log (userInput);
+
+
+var userInput = 'max' 
+//let userInput = 'max'
